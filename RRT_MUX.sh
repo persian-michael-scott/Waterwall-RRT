bc570bf2290e90d902d0dc22c9cea16b86fb5512
Rasoul Khajavi <tot109.rk@gmail.com>
2025-05-21 21:40:48 +0330

    added number of worker input

--- RRT_MUX.sh
+++ RRT_MUX.sh
@@ -159,6 +159,7 @@ while true; do
             echo "Please answer yes (y) or no (n)."
             break
         fi
+        read -p "Enter Workers : " worker
 
         cat > core.json << EOF
 {
@@ -182,7 +183,7 @@ while true; do
     },
     "dns": {},
     "misc": {
-        "workers": 0,
+        "workers": $worker,
         "ram-profile": "server",
         "libs-path": "libs/"
     },
@@ -195,102 +196,100 @@ EOF
 
     if [ "$choice" -eq 1 ]; then
         public_ip=$(wget -qO- https://api.ipify.org)
-        echo "Iran IPv4/6 is: $public_ip"
-        read -p "enter Kharej Ipv4: " ip_remote
+        echo "Iran IPv4 is: $public_ip"
+        read -p "enter Kharej Ipv4/6: " ip_remote
         read -p "Enter the SNI (default: sahab.ir): " input_sni
         HOSTNAME=${input_sni:-sahab.ir}
         cat > config.json << EOF
 {
-    "name": "reverse_reality_grpc_hd_multiport_server",
-    "nodes": [
-        {
-            "name": "users_inbound",
-            "type": "TcpListener",
-            "settings": {
-                "address": "0.0.0.0",
-                "port": [23,65535],
-                "nodelay": true
-            },
-            "next": "header"
-        },
-        {
-            "name": "header",
-            "type": "HeaderClient",
-            "settings": {
-                "data": "src_context->port"
-            },
-            "next": "bridge2"
-        },
-        {
-            "name": "bridge2",
-            "type": "Bridge",
-            "settings": {
-                "pair": "bridge1"
-            }
-        },
-        {
-            "name": "bridge1",
-            "type": "Bridge",
-            "settings": {
-                "pair": "bridge2"
-            }
-        },
-        {
-            "name": "reverse_server",
-            "type": "ReverseServer",
-            "settings": {},
-            "next": "bridge1"
-        },
-        {
-            "name": "pbserver",
-            "type": "ProtoBufServer",
-            "settings": {},
-            "next": "reverse_server"
-        },
-        {
-            "name": "h2server",
-            "type": "Http2Server",
-            "settings": {},
-            "next": "pbserver"
-        },
-        {
-            "name": "halfs",
-            "type": "HalfDuplexServer",
-            "settings": {},
-            "next": "h2server"
-        },
-        {
-            "name": "reality_server",
-            "type": "RealityServer",
-            "settings": {
-                "destination": "reality_dest",
-                "password": "passwd"
-            },
-            "next": "halfs"
-        },
-        {
-            "name": "kharej_inbound",
-            "type": "TcpListener",
-            "settings": {
-                "address": "0.0.0.0",
-                "port": 443,
-                "nodelay": true,
-                "whitelist": [
-                    "$ip_remote/32"
-                ]
-            },
-            "next": "reality_server"
-        },
-        {
-            "name": "reality_dest",
-            "type": "TcpConnector",
-            "settings": {
-                "nodelay": true,
-                "address": "$HOSTNAME",
-                "port": 443
-            }
-        }
-    ]
+  "name": "reverse_reality_grpc_hd_multiport_server",
+  "nodes": [
+    {
+      "name": "users_inbound",
+      "type": "TcpListener",
+      "settings": {
+        "address": "0.0.0.0",
+        "port": [23, 65535],
+        "nodelay": true
+      },
+      "next": "header"
+    },
+    {
+      "name": "header",
+      "type": "HeaderClient",
+      "settings": {
+        "data": "src_context->port"
+      },
+      "next": "bridge2"
+    },
+    {
+      "name": "bridge2",
+      "type": "Bridge",
+      "settings": {
+        "pair": "bridge1"
+      }
+    },
+    {
+      "name": "bridge1",
+      "type": "Bridge",
+      "settings": {
+        "pair": "bridge2"
+      }
+    },
+    {
+      "name": "reverse_server",
+      "type": "ReverseServer",
+      "settings": {},
+      "next": "bridge1"
+    },
+    {
+      "name": "pbserver",
+      "type": "ProtoBufServer",
+      "settings": {},
+      "next": "reverse_server"
+    },
+    {
+      "name": "h2server",
+      "type": "Http2Server",
+      "settings": {},
+      "next": "pbserver"
+    },
+    {
+      "name": "halfs",
+      "type": "HalfDuplexServer",
+      "settings": {},
+      "next": "h2server"
+    },
+    {
+      "name": "reality_server",
+      "type": "RealityServer",
+      "settings": {
+        "destination": "reality_dest",
+        "password": "passwdrasoul!"
+      },
+      "next": "halfs"
+    },
+    {
+      "name": "kharej_inbound",
+      "type": "TcpListener",
+      "settings": {
+        "address": "0.0.0.0",
+        "port": 443,
+        "nodelay": true,
+        "whitelist": ["$ip_remote/32"]
+      },
+      "next": "reality_server"
+    },
+    {
+      "name": "reality_dest",
+      "type": "TcpConnector",
+      "settings": {
+        "nodelay": true,
+        "address": "$HOSTNAME",
+        "port": 443
+      }
+    }
+  ]
 }
 EOF
         sleep 0.5
@@ -309,92 +308,91 @@ EOF
         HOSTNAME=${input_sni:-sahab.ir}
         cat > config.json << EOF
 {
-    "name": "reverse_reality_grpc_client_hd_multiport_client",
-    "nodes": [
-        {
-            "name": "outbound_to_core",
-            "type": "TcpConnector",
-            "settings": {
-                "nodelay": true,
-                "address": "127.0.0.1",
-                "port": "dest_context->port"
-            }
-        },
-        {
-            "name": "header",
-            "type": "HeaderServer",
-            "settings": {
-                "override": "dest_context->port"
-            },
-            "next": "outbound_to_core"
-        },
-        {
-            "name": "bridge1",
-            "type": "Bridge",
-            "settings": {
-                "pair": "bridge2"
-            },
-            "next": "header"
-        },
-        {
-            "name": "bridge2",
-            "type": "Bridge",
-            "settings": {
-                "pair": "bridge1"
-            },
-            "next": "reverse_client"
-        },
-        {
-            "name": "reverse_client",
-            "type": "ReverseClient",
-            "settings": {
-                "minimum-unused": 16
-            },
-            "next": "pbclient"
-        },
-        {
-            "name": "pbclient",
-            "type": "ProtoBufClient",
-            "settings": {},
-            "next": "h2client"
-        },
-        {
-            "name": "h2client",
-            "type": "Http2Client",
-            "settings": {
-                "host": "$HOSTNAME",
-                "port": 443,
-                "path": "/",
-                "contenttype": "application/grpc",
-                "concurrency": 64
-            },
-            "next": "halfc"
-        },
-        {
-            "name": "halfc",
-            "type": "HalfDuplexClient",
-            "next": "reality_client"
-        },
-        
-        {
-            "name": "reality_client",
-            "type": "RealityClient",
-            "settings": {
-                "sni": "$HOSTNAME",
-                "password": "passwd"
-            },
-            "next": "outbound_to_iran"
-        },
-        {
-            "name": "outbound_to_iran",
-            "type": "TcpConnector",
-            "settings": {
-                "nodelay": true,
-                "address": "$ip_remote",
-                "port": 443
-            }
-        }
-    ]
+  "name": "reverse_reality_grpc_client_hd_multiport_client",
+  "nodes": [
+    {
+      "name": "outbound_to_core",
+      "type": "TcpConnector",
+      "settings": {
+        "nodelay": true,
+        "address": "127.0.0.1",
+        "port": "dest_context->port"
+      }
+    },
+    {
+      "name": "header",
+      "type": "HeaderServer",
+      "settings": {
+        "override": "dest_context->port"
+      },
+      "next": "outbound_to_core"
+    },
+    {
+      "name": "bridge1",
+      "type": "Bridge",
+      "settings": {
+        "pair": "bridge2"
+      },
+      "next": "header"
+    },
+    {
+      "name": "bridge2",
+      "type": "Bridge",
+      "settings": {
+        "pair": "bridge1"
+      },
+      "next": "reverse_client"
+    },
+    {
+      "name": "reverse_client",
+      "type": "ReverseClient",
+      "settings": {
+        "minimum-unused": 16
+      },
+      "next": "pbclient"
+    },
+    {
+      "name": "pbclient",
+      "type": "ProtoBufClient",
+      "settings": {},
+      "next": "h2client"
+    },
+    {
+      "name": "h2client",
+      "type": "Http2Client",
+      "settings": {
+        "host": "$HOSTNAME",
+        "port": 443,
+        "path": "/",
+        "content-type": "application/grpc",
+        "concurrency": 64
+      },
+      "next": "halfc"
+    },
+    {
+      "name": "halfc",
+      "type": "HalfDuplexClient",
+      "next": "reality_client"
+    },
+    {
+      "name": "reality_client",
+      "type": "RealityClient",
+      "settings": {
+        "sni": "$HOSTNAME",
+        "password": "passwdrasoul!"
+      },
+      "next": "outbound_to_iran"
+    },
+    {
+      "name": "outbound_to_iran",
+      "type": "TcpConnector",
+      "settings": {
+        "nodelay": true,
+        "address": "$ip_remote",
+        "port": 443
+      }
+    }
+  ]
 }
 EOF
         sleep 0.5
